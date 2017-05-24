da7fe7a9e50d5bc12103032f3276127e95be9d81
Louis Jenkins <LouisJenkinsCS@hotmail.com>
2017-05-23 20:01:32 -0400

    I think I got it! Non-Blocking reads and writes *should* be correct enough for the MCAS algorithm to be scalable.

--- src/mcas.c
+++ src/mcas.c
@@ -12,6 +12,11 @@
 
 typedef uint64_t word_t;
 
+// Potential modification: Have the "expected" and "new" be apart of a union
+// and add specific types for READ/WRITE/CAS...
+// CAS = {e, n}
+// Write = {n}
+// Read = {r, n}
 typedef struct {
   // Number of logical locations
   int N;
@@ -34,9 +39,44 @@ static void AddressSort(MCASDesc *d);
 // We use descriptors so that MCASHelper can allow recursive helping.
 bool MCAS(int N, word_t **a, word_t *e, word_t *n);
 
-// Note that MCASRead will ONLY work if the address being read is manage properly
+// Note that MCASRead will ONLY work if the address being read is managed properly
 // through the MCASDesc abstraction!
-word_t MCASRead(word_t *a);
+// Idea:
+//  curr <- d->a[i]
+//  if isMCASDesc(curr) then
+//    MCASHelp(curr)
+//    continue
+//  d->n[i] = curr;
+//  if CCAS(d->a[i], curr, d, &d->status) then
+//    ...
+//
+// Later on in MCASHelper when we're performing the decision point for a reader...
+//  for (int i = 0; i < d->N; i++)
+//    curr <- d->n[i];
+//    if CAS(d->a[i], d, d->n[i]) then
+//      d->r[i] = curr;
+//
+// The above guarantees forward progression in a non-blocking fashion for reads,
+// and writes theh same value back after the descriptor is finished.
+word_t *MCASRead(word_t *a, int N);
+
+// Note that MCASWrite will ONLY work if the address being written is managed properly
+// through the MCASDesc abstraction!
+// Idea:
+//  curr <- d->a[i]
+//  if isMCASDesc(curr) then
+//    MCASHelp(curr)
+//    continue
+//  if CCAS(d->a[i], curr, d, &d->status) then
+//    ...
+//
+// Later on in MCASHelper when we're performing the decision point for a reader...
+//  for (int i = 0; i < d->N; i++)
+//    CAS(d->a[i], d, d->n[i])
+//
+// The above guarantees forward progression in a non-blocking fashion for reads,
+// and writes theh same value back after the descriptor is finished.
+void MCASWrite(word_t *a, int N);
 
 // A recursive helper algorithm; each thread will attempt to set their descriptor to
 // claim ownership of a contested location; if another descriptor is encountered,
