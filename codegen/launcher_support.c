e30f5d12ee6f75b58d186f1d6e991488e4e3c560
Louis Jenkins <LouisJenkinsCS@hotmail.com>
2017-07-07 14:35:42 -0400

    Remoted codegen so .gitignore can safely ignore it

--- codegen/launcher_support.c
+++
@@ -1,6 +0,0 @@
-#include "chplcgfns.h"
-#include "config.h"
-#include "_config.c"
-#include "chpl_compilation_config.c"
-const char launcher_real_suffix[] = "_real";
-const char launcher_exe_suffix[] = "";
